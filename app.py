17a994cd21d13984e6fcc3bae90f36fe091d42a3
Jyotirmay Kumar Jha <jyotirmay18.jk@gmail.com>
2024-09-23 23:43:12 +0530

    routes

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    # return("HOMEPAGE")
     return render_template('index.html')
 
 @app.route("/register")
 def registration():
-    return(render_template("index.html"))
+    return(render_template("regForm.html"))
 
 @app.route("/submitregn", methods=["POST"])
 def submit():
@@ -17,7 +17,16 @@ def submit():
 
 @app.route("/teams")
 def teaminfo():
-    return("TEAMS")
+    files = os.walk("./static/assets/heads")
+    data = list(files)[0][2]
+    dic = {"tech":[],
+           "pr":[],
+           "exec":[]}
+    for ele in data:
+        team, name = ele.split('-')
+        dic[team].append({"name":name.split('.')[0].upper(),
+                          "src": f"/static/assets/heads/{ele}"})
+    return render_template("teams.html",heads = dic)
 
 if __name__ == "__main__":
     app.run()
